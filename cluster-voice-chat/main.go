b2e0a9349d959b1423cf7a5355c983cf7f77757e
Amine Hilaly <hilalyamine@gmail.com>
2023-11-06 09:40:43 -0600

    Add moar stuff

--- cluster-voice-chat/main.go
+++ cluster-voice-chat/main.go
@@ -52,21 +52,36 @@ func main() {
 		m := scanner.Text()
 		allOutput = append(allOutput, m)
 	}
-	fmt.Println(getFullSentence(allOutput))
+	// fmt.Println(getFullSentence(allOutput))
 	myAudioInputFile := "my-audio-input.yaml"
-	_ = writeOutputToKubernetesCommandFile(myAudioInputFile, getFullSentence(allOutput))
+	s, err := getFullSentence(allOutput)
+	if err != nil {
+		fmt.Println("error getting the full sentence: ", err)
+		return
+	}
+	_ = writeOutputToKubernetesCommandFile(myAudioInputFile, s)
+
+	kubectlCmd := exec.Command("kubectl", "apply", "-f", myAudioInputFile)
+	kubectlCmd.Stdout = os.Stdout
+	kubectlCmd.Stderr = os.Stderr
+	kubectlCmd.Stdin = os.Stdin
+	err = kubectlCmd.Run()
+	if err != nil {
+		fmt.Println("error running the command: ", err)
+	}
 }
 
-func getFullSentence(allOutput []string) string {
+func getFullSentence(allOutput []string) (string, error) {
 	// traverse the array in reverse order and find the first
 	// element that matches the last element.
 	lastElement := allOutput[len(allOutput)-1]
 	for i := len(allOutput) - 2; i >= 0; i-- {
 		if allOutput[i] == lastElement {
-			return strings.Join(allOutput[i+1:], " ") + "."
+			return strings.Join(allOutput[i+1:], " ") + ".", nil
 		}
 	}
-	return "ERROR"
+	fmt.Println(lastElement)
+	return "ERROR", fmt.Errorf("could not find the last element")
 }
 
 // writeOutputToKubernetesCommandFile writes the output to a kubernetes command file
@@ -81,10 +96,20 @@ spec:
 */
 func writeOutputToKubernetesCommandFile(filename, input string) error {
 	content := fmt.Sprintf(`apiVersion: batch.yolo.ahilaly.dev/v1alpha1
-kind: Command
+kind: CommandExec
 metadata:
-	name: my-command
+    name: my-command-%s
 spec:
-	input: %s`, input)
+    input: %s`, randomString(5), input)
 	return os.WriteFile(filename, []byte(content), 0644)
+}
+
+func randomString(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyz" +
+		"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[0]
+	}
+	return string(b)
 }
--- go.mod
+++ go.mod
@@ -1,11 +1,13 @@
 module github.com/yolo-operator/yolo-operator
 
-go 1.20
+go 1.21
+
+toolchain go1.21.1
 
 require (
-	github.com/aws/aws-sdk-go v1.47.0
-	github.com/onsi/ginkgo/v2 v2.9.5
-	github.com/onsi/gomega v1.27.7
+	github.com/go-skynet/go-llama.cpp v0.0.0-20231009155254-aeba71ee8428
+	github.com/onsi/ginkgo/v2 v2.13.0
+	github.com/onsi/gomega v1.28.0
 	github.com/sashabaranov/go-openai v1.16.0
 	k8s.io/api v0.27.2
 	k8s.io/apimachinery v0.27.2
@@ -51,13 +53,13 @@ require (
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/oauth2 v0.5.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/term v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
-	golang.org/x/tools v0.9.1 // indirect
+	golang.org/x/tools v0.12.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.3.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
